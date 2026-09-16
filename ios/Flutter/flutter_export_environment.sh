acd147b8dfa05d61da844814914142e4985c4331
Nam4eb <leethanhlong7890@gmail.com>
2026-09-16 17:29:57 +0700

    build apk

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\APP\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\PROJECT\SquadUp"
-export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=D:\PROJECT\SquadUp\ios\Flutter\ephemeral\Packages\.packages\FlutterFramework"
+export "FLUTTER_ROOT=D:\App\SDK\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Project\SquadUp"
+export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=D:\Project\SquadUp\ios\Flutter\ephemeral\Packages\.packages\FlutterFramework"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
